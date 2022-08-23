5893d321bf64817a702588a67cd3ab04d42d8f97
ananconda <onefire430@outlook.com>
2022-08-22 22:26:19 -0400

    minor change for BaseToken

--- contracts/BaseERC20.sol
+++ contracts/BaseERC20.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BaseERC20 is ERC20, Ownable {
-    uint256 public tokenTotalSupply = 1000000 * 10**18;
+    uint256 public tokenTotalSupply = 100000000 * 10**18;
 
     constructor() ERC20("BetCoin", "BetCoin") {
         _mint(msg.sender, tokenTotalSupply);
