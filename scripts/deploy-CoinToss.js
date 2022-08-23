3e818711c5e06f267468681d879fb94ffb3f6ecc
ananconda <onefire430@outlook.com>
2022-08-22 22:27:21 -0400

    no message

--- scripts/deploy-CoinToss.js
+++ scripts/deploy-CoinToss.js
@@ -5,7 +5,6 @@ async function main() {
 
 	const CoinToss = await ethers.getContractFactory("CoinToss");
 	console.log("Deploying...");
-	// const box = await CoinToss.deploy("0x094616f0bdfb0b526bd735bf66eca0ad254ca81f");
 	const box = await CoinToss.deploy("0xCE301566b668E7Fa0F207655671f873370b925a3");
 	console.log("COINTOSS :", box.address);
 }
